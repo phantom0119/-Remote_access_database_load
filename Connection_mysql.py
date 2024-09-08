04b1bd053e35162a1c517cb180b0947b5b96fe82
phantom0119 <phantom0119@daum.net>
2024-09-08 20:26:29 +0900

    update: OpenSSL 작업 기록 0908

--- Connection_mysql.py
+++ Connection_mysql.py
@@ -1,6 +1,14 @@
 # pip install mysql-connector-python
 import mysql.connector as  mycon
 
+def mysql_connection(myid, mypw):
+    connection = mycon.connect(
+        host='192.168.3.4',
+        user=myid,
+        password=mypw
+    )
+
+
 
 
 if __name__ == "__main__":
